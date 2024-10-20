4f7b04ed5ad9d6670b47d3ef0b45a77246fbfd59
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-10-20 19:55:44 +0200

    add styling to start screen

--- app/src/main/java/com/example/missingpiece/Start.kt
+++ app/src/main/java/com/example/missingpiece/Start.kt
@@ -1,33 +1,70 @@
 package com.example.missingpiece
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.missingpiece.ui.theme.Blue10
+import com.example.missingpiece.ui.theme.Blue40
 import com.example.missingpiece.ui.theme.MissingPieceTheme
+import com.example.missingpiece.ui.theme.Orange40
 
 @Composable
 fun Start(
     goToGame : () -> Unit,
     goToHighScore : () -> Unit,
-    goToInstructions: () -> Unit){
-    Column {
-        Button(onClick = {
-            goToGame()
-        }) {
+    goToInstructions: () -> Unit,
+    modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = Blue10),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+        Button(
+            onClick = { goToGame()},
+            colors = ButtonDefaults
+                .buttonColors(containerColor = Blue40),
+            border = BorderStroke(2.dp, color = Orange40),
+            modifier = Modifier
+                .width(width = 250.dp)
+            ) {
             Text("START GAME")
         }
 
-        Button(onClick = {
-            goToHighScore()
-        }) {
+        Button(
+            onClick = { goToHighScore()},
+            colors = ButtonDefaults
+                .buttonColors(containerColor = Blue40),
+            border = BorderStroke(2.dp, color = Orange40),
+            modifier = Modifier
+                .width(width = 250.dp)
+
+        ) {
             Text("HIGH SCORE")
         }
 
-        Button(onClick = {
-            goToInstructions()
-        }) {
+        Button(
+            onClick = { goToInstructions() },
+            colors = ButtonDefaults
+                .buttonColors(containerColor = Blue40),
+            border = BorderStroke(2.dp, color = Orange40),
+            modifier = Modifier
+                .width(width = 250.dp)
+            ) {
             Text("INSTRUCTIONS")
         }
 
--- app/src/main/java/com/example/missingpiece/ui/theme/Color.kt
+++ app/src/main/java/com/example/missingpiece/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Blue10 = Color(red=38, green=181, blue=181)
+val Blue40 = Color(red=28, green=148, blue=148)
+val Orange40 = Color(red=183, green=87, blue=18)

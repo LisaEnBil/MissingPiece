835fe07bae3d1a0a084dd5e93ab40a2801c5c9fa
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-10-21 06:24:33 +0200

    add styling to other screens

--- app/src/main/java/com/example/missingpiece/Game.kt
+++ app/src/main/java/com/example/missingpiece/Game.kt
@@ -1,13 +1,25 @@
 package com.example.missingpiece
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.missingpiece.ui.theme.Blue10
 import com.example.missingpiece.ui.theme.MissingPieceTheme
 
 @Composable
 fun Game(){
-    Text(text = "Game")
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = Color.Black),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){  Text(text = "Game", color = Blue10) }
 }
 
 

024fab8755e8350e365456ea2be5bc34b87e3d0b
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-10-31 08:07:54 +0100

    add function to handle changing difficulty levels

--- app/src/main/java/com/example/missingpiece/GameViewModel.kt
+++ app/src/main/java/com/example/missingpiece/GameViewModel.kt
@@ -1,15 +1,24 @@
 package com.example.missingpiece
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    private val _difficulty = mutableIntStateOf(3)
+    val difficulty: State<Int> = _difficulty
+
     private val _hasOngoingGame = mutableStateOf(false)
+
     val hasOngoingGame: State<Boolean> = _hasOngoingGame
 
     private var savedGameState: List<List<Int>>? = null
+
+    fun setDifficulty(gridSize: Int) {
+        _difficulty.intValue = gridSize
+    }
 
     fun saveGameState(gameState: List<List<Int>>) {
         savedGameState = gameState

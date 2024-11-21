7155fd4aa058316b6b01abba8f5930d8eba34e97
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-11-21 18:38:20 +0100

    add new viewModel

--- app/src/main/java/com/example/missingpiece/MainActivity.kt
+++ app/src/main/java/com/example/missingpiece/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.missingpiece
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,11 +25,17 @@ enum class NavScreen() {
 
 class MainActivity : ComponentActivity() {
     private val viewModel: GameViewModel by viewModels()
-
+    private val hsViewModel: HighScoreViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return HighScoreViewModel(applicationContext) as T
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyAppNavHost(viewModel)
+            MyAppNavHost(viewModel, hsViewModel)
         }
     }
 }
@@ -33,9 +43,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyAppNavHost(
     viewModel: GameViewModel,
+    hsViewModel: HighScoreViewModel,
     navController: NavHostController = rememberNavController(),
 ) {
-    val context = LocalContext.current
 
     NavHost(navController = navController, startDestination = NavScreen.Start.name) {
         composable(NavScreen.Start.name) {
@@ -49,12 +59,13 @@ fun MyAppNavHost(
         composable(NavScreen.Game.name) {
             Game(
                 viewModel = viewModel,
+                hsViewModel = hsViewModel,
                 goToGame = { navController.navigate(NavScreen.Game.name) },
                 onBackPressed = { navController.popBackStack() },
                 goToStart = { navController.navigate(NavScreen.Start.name) });
         }
         composable(NavScreen.HighScore.name) {
-            HighScore(viewModel = viewModel, context);
+            HighScore(hsViewModel = hsViewModel);
         }
         composable(NavScreen.Settings.name) {
             Settings( viewModel = viewModel);

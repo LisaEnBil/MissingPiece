9c477d917442342de5ae9c82e305eebfdc8b367b
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-11-21 18:39:06 +0100

    Create db

---
+++ app/src/main/java/com/example/missingpiece/HighScoreDatabase.kt
@@ -0,0 +1,64 @@
+package com.example.missingpiece
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.ui.platform.LocalContext
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val uid: Int,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "score") val score: Int?,
+    @ColumnInfo(name = "size") val size: Int?
+)
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user ORDER BY score ASC LIMIT 10")
+    fun getTopTenUsers(): List<User>
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+}
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context?): AppDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context!!,
+                    AppDatabase::class.java,
+                    "database-name"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+
+}
